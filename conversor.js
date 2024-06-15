ebbe4f087ae788ae7e19d40565d5dc84ba84ead1
David Vásquez <cdavidvasquezr20@gmail.com>
2024-06-14 20:49:32 -0600

    create a new file for the converter

--- conversor.js
+++ conversor.js
@@ -1,46 +1,6 @@
 let  cociente, residuo, numeroSalida, resultado;
-const $btn = document.querySelector("button");
-const $decimal = document.querySelector("input");
-const $valueBinario = document.querySelector(".binario")
-const $valueOctal = document.querySelector(".octal")
-const $valueHexa = document.querySelector(".hexa")
-const $error = document.querySelector("p")
 
-$btn.addEventListener("click", (e)=>{
-    e.preventDefault()
-    let numDecimal = $decimal.value
-    if(numDecimal){
-        if (numDecimal % 1 == 0){
-            numDecimal = parseInt($decimal.value)
-            $valueBinario.innerHTML = `${CONVERSOR(numDecimal, 2)}`
-            $valueOctal.innerHTML = `${CONVERSOR(numDecimal, 8)}`
-            $valueHexa.innerHTML = `${CONVERSOR(numDecimal, 16)}`
-        } else{
-            $error.classList.add("error")
-            $error.innerHTML = "Por favor escriba un numero entero"
-            $error.classList.add("error")
-            setTimeout(()=>{
-            $error.classList.remove("error")
-            }, 2000)
-   
-        }
-        return
-    }
-
-    if(!numDecimal){
-        $error.classList.add("error")
-        $error.innerHTML = "Por favor ingrese un número"
-        $error.classList.add("error")
-        setTimeout(()=>{
-            $error.classList.remove("error")
-        }, 2000)
-        return
-    }
-    $decimal.value = ""
-})  
-
-
-const CONVERSOR = (numero, base)=>{
+export const CONVERSOR = (numero, base)=>{
     numeroSalida = [];
 
     while (numero >= base){
@@ -63,7 +23,7 @@ const CONVERSOR = (numero, base)=>{
 }
 
 const validarHexa = (residuo)=>{
-    for(i = residuo; i <= residuo; i++){
+    for(let i = residuo; i <= residuo; i++){
         let ascii = 55 + residuo;
         return String.fromCharCode(ascii)
     } 
